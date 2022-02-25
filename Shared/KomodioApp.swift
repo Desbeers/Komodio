d9b6a85eb7ed84dc33b718343f06a028db48137f
Nick Berendsen <nick@desbeers.nl>
2022-02-25 21:40:29 +0100

    Initial Commit

---
+++ Shared/KomodioApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomodioApp.swift
+//  Shared
+//
+//  Created by Nick Berendsen on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct KomodioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
