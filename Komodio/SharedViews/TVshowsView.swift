500ea61a911c56c93591f4aef4e06f9d04e34826
Nick Berendsen <nick@desbeers.nl>
2022-03-07 23:42:36 +0000

    Update TVshowsView.swift

--- Komodio/SharedViews/TVshowsView.swift
+++ Komodio/SharedViews/TVshowsView.swift
@@ -34,6 +34,7 @@ extension TVshowsView {
         /// The View
         var body: some View {
             RouterLink(item: .episodes(tvshow: tvshow)) {
+                Text(tvshow.episode.description)
                 ItemsView.Basic(item: tvshow.binding())
             }
             .buttonStyle(ButtonStyles.MediaItem(item: tvshow))
